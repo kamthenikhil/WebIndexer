feb9adda9a292d892b841ec9d531357f9bd95aa2
Nikhil Kamthe <kamthenikhil@gmail.com>
2015-11-29 13:21:14 -0800

    Refactoring..

--- WebIndexer/src/com/chinappa/indexer/WebIndexer.java
+++ WebIndexer/src/com/chinappa/indexer/WebIndexer.java
@@ -5,12 +5,9 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.FieldType;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.FieldInfo.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -19,7 +16,6 @@ import org.apache.lucene.util.Version;
 import org.jsoup.Jsoup;
 
 import com.chinappa.indexer.configuration.IndexerConfiguration;
-import com.chinappa.indexer.constant.IndexerConstants;
 import com.chinappa.information.retrieval.constant.CommonConstants;
 import com.chinappa.information.retrieval.util.FileHandlerUtil;
 
@@ -105,43 +101,22 @@ public class WebIndexer {
 			String title, String metadata, String content, String anchorText,
 			String url) throws IOException {
 		Document doc = new Document();
-		FieldType storedFieldWithTermVectors = new FieldType(
-				TextField.TYPE_STORED);
-		storedFieldWithTermVectors.setIndexed(true);
-		storedFieldWithTermVectors
-				.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
-		storedFieldWithTermVectors.setStoreTermVectors(true);
-		storedFieldWithTermVectors.setStoreTermVectorPositions(true);
-
-		FieldType unstoredFieldWithTermVectors = new FieldType(
-				TextField.TYPE_NOT_STORED);
-		unstoredFieldWithTermVectors.setIndexed(true);
-		unstoredFieldWithTermVectors
-				.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
-		unstoredFieldWithTermVectors.setStoreTermVectors(true);
-		unstoredFieldWithTermVectors.setStoreTermVectorPositions(true);
-
-		doc.add(new Field(CommonConstants.TITLE_FIELD, title,
-				storedFieldWithTermVectors));
-		doc.add(new Field(CommonConstants.METADATA_FIELD, metadata,
-				unstoredFieldWithTermVectors));
-		doc.add(new Field(CommonConstants.CONTENT_FIELD, content,
-				unstoredFieldWithTermVectors));
-		doc.add(new Field(CommonConstants.ANCHOR_TEXT_FIELD, anchorText,
-				unstoredFieldWithTermVectors));
-		doc.add(new Field(CommonConstants.INDEX_FIELD, url,
-				storedFieldWithTermVectors));
+		doc.add(new Field(CommonConstants.TITLE_FIELD, title , Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS));
+		doc.add(new Field(CommonConstants.METADATA_FIELD, metadata , Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS));
+		doc.add(new Field(CommonConstants.CONTENT_FIELD, content , Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS));
+		doc.add(new Field(CommonConstants.ANCHOR_TEXT_FIELD, anchorText , Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS));
+		doc.add(new Field(CommonConstants.INDEX_FIELD, url , Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS));
 		indexWriter.addDocument(doc);
 	}
 
 	private IndexWriterConfig getIndexWriterConfig() {
-		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_41,
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36,
 				getAnalyzer());
 		return config;
 	}
 
 	private Analyzer getAnalyzer() {
-		Analyzer analyzer = new StopAnalyzer(Version.LUCENE_41);
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_36);
 		return analyzer;
 	}
 
