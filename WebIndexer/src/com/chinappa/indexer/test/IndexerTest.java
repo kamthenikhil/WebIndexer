bae0ea0d56374495bd4fd19bae053f73eaf8d9ef
Nikhil Kamthe <kamthenikhil@gmail.com>
2015-12-03 07:01:46 -0800

    Submitted Version

--- WebIndexer/src/com/chinappa/indexer/test/IndexerTest.java
+++ WebIndexer/src/com/chinappa/indexer/test/IndexerTest.java
@@ -9,7 +9,6 @@ public class IndexerTest {
 		long startTime = System.currentTimeMillis();
 		WebIndexer indexer = new WebIndexer();
 		indexer.buildIndex();
-		//indexer.searchDocuments("davis");
 		long stopTime = System.currentTimeMillis();
 		long elapsedTime = stopTime - startTime;
 		System.out.println("Time elapsed: " + elapsedTime / 1000 + " secs");
